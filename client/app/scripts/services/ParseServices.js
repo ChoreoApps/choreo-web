039271ca3a99aee222cb7f39a563290b34f267cc
zasmail <zasmail@utexas.edu>
2014-08-29 16:23:15 -0500

    Added facebook keys

--- client/app/scripts/services/ParseServices.js
+++ client/app/scripts/services/ParseServices.js
@@ -3,14 +3,14 @@ angular.module('ParseServices', [])
 
   // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
   Parse.initialize("oSTqi09K98dnrzBekSEPv3SH3pLG4mzDUg7BB92F", "2h20chQvEdFdOQREVqe141a9FxYXvZ3mnmGdfvAY");
-
+  //Secret key: 88bca3f9c73863a34e6a0349a902d3d6
   // FACEBOOK init
   window.fbPromise.then(function() {
 
     Parse.FacebookUtils.init({
 
       // pro-tip: swap App ID out for PROD App ID automatically on deploy using grunt-replace
-      appId: 481650395275919, // Facebook App ID
+      appId: 337308366446159, // Facebook App ID
       channelUrl: 'http://brandid.github.io/parse-angular-demo/channel.html', // Channel File
       cookie: true, // enable cookies to allow Parse to access the session
       xfbml: true, // parse XFBML
